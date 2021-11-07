2f4f36488dd3b7923f19f9add30b3abe17bb8f33
Gabriel Keilty <gabrielpkeilty@gmail.com>
2021-11-07 17:10:28 -0300

    subo tp formularios

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
+const port = 3031;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'home.html'));
-});
+app.listen(port, () => console.log(`Servidor corriendo en el puerto http://localhost:${port}`));
 
-const port = 3030;
-
-app.listen(port, () => console.log('Servidor corriendo en el puerto', port));
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+app.get('/register',(req,res) => res.sendFile(path.join(__dirname,'views','register.html')));
+app.get('/login',(req,res) => res.sendFile(path.join(__dirname,'views','login.html')));
